c5567cd2edc22d82873ea7488c28d41dbcabbc9f
felipeluix32 <luiz.felipe.oliveira.nascimento@gmail.com>
2022-06-27 18:50:37 -0300

    SellerService and FindAll

---
+++ Controllers/SellersController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Services;
+
+namespace SalesWebMvc.Controllers
+{
+    public class SellersController : Controller
+    {
+        private readonly SellerService _selllerService;
+
+        public SellersController(SellerService selllerService)
+        {
+            _selllerService = selllerService;
+        }
+
+        public IActionResult Index()
+        {
+            var list = _selllerService.FindAll();
+            return View(list);
+        }
+    }
+}
