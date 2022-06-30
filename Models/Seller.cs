64095d1041cc96b7719fe228162ce2f9140e76f0
felipeluix32 <luiz.felipe.oliveira.nascimento@gmail.com>
2022-06-30 16:25:42 -0300

    Validation

--- Models/Seller.cs
+++ Models/Seller.cs
@@ -8,16 +8,21 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
+        
+        [Required]
         public string Name { get; set; }
-
+        
+        [Required]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
         
+        [Required]
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
-
+       
+        [Required]
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
